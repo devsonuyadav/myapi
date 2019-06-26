78f37b0cc160b4e210d5fe10fdb1b93e955de705
sky32752 <sky32752@gmail.com>
2019-06-26 21:16:57 +0530

    something

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var { mongoose } = require('./db/mongoose');
 var { Todo, bio } = require('./models/todo');
 var { User } = require('./models/user');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 
