3bd76e23adb5afa31c1f96c87b8fb8a51671ef30
sky32752 <sky32752@gmail.com>
2019-06-27 22:26:17 +0530

    dsd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://sky32752:Skyf!zz143@cluster0-b1on8.mongodb.net/test?retryWrites=true&w=majority"|| "mongodb://localhost:27017/TodoApp" , {useNewUrlParser : true})
-
+mongoose.connect(process.env.MONGODB_URI , {useNewUrlParser : true})
+process.env.MONGODB_URI === "mongodb+srv://sky32752:Skyf!zz143@cluster0-b1on8.mongodb.net/test?retryWrites=true&w=majority"
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
